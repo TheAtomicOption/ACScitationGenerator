ccad79f27823fa19518237b6322b9c59a9b953ed
TheAtomicOption <orbenn@hotmail.com>
2014-05-09 23:07:33 -0700

    CSS and radio buttons
    Added a CSS and radio buttons that don't do anything yet..

--- ACSgenerator.js
+++ ACSgenerator.js
@@ -15,44 +15,43 @@
     /* setupUpdater will be called once, on page load.*/
 
     function setupUpdater(){
-    var lastname=document.getElementById('lastname')
-    , title=document.getElementById('title')
-    , year=document.getElementById('year')
-    , FullCitation=document.getElementById('FullCitation')
-    , InTextCitation=document.getElementById('InTextCitation')
-    , oldText=FullCitation.value
-    , oldInText=InTextCitation.value
-    , timeout=null;
+        var lastname=document.getElementById('lastname')
+        , title=document.getElementById('title')
+        , year=document.getElementById('year')
+        , FullCitation=document.getElementById('FullCitation')
+        , InTextCitation=document.getElementById('InTextCitation')
+        , oldText=FullCitation.value
+        , oldInText=InTextCitation.value
+        , timeout=null;
 
       /* handleChange is called 50ms after the user stops
       typing. */
 
-      function handleChange(){
-      var newCitation=lastname.value + ' ' + title.value + ' ' + year.value;
-      if (newCitation==oldText) return; else oldText=newCitation;
-      set(FullCitation, newCitation);
+          function handleChange(){
+              var newCitation=lastname.value + ' ' + title.value + ' ' + year.value;
+              if (newCitation==oldText) return; else oldText=newCitation;
+              set(FullCitation, newCitation);
 
-      var newInText='('+lastname.value.substr(0,replaceNull(lastname.value.indexOf(';'),lastname.value.length))
-      +', ' + year.value + ')';
+              var newInText='('+lastname.value.substr(0,replaceNull(lastname.value.indexOf(';'),lastname.value.length))
+              +', ' + year.value + ')';
 
-      /*if (newInText==oldInText) return; else oldInText=newInText;*/
-      if (newInText==oldInText) return; else oldInText=newInText;
-      set(InTextCitation, newInText);
+              /*if (newInText==oldInText) return; else oldInText=newInText;*/
+              if (newInText==oldInText) return; else oldInText=newInText;
+              set(InTextCitation, newInText);
+          }
 
-      }
 
+          /* eventHandler is called on keyboard and mouse events.
+          If there is a pending timeout, it cancels it.
+          It sets a timeout to call handleChange in 50ms. */
+          function eventHandler(){
+          if(timeout) clearTimeout(timeout);
+          timeout=setTimeout(handleChange, 50);
+          }
 
-      /* eventHandler is called on keyboard and mouse events.
-      If there is a pending timeout, it cancels it.
-      It sets a timeout to call handleChange in 50ms. */
-      function eventHandler(){
-      if(timeout) clearTimeout(timeout);
-      timeout=setTimeout(handleChange, 50);
-      }
-
-    lastname.onkeydown=lastname.onkeyup=lastname.onclick=
-    title.onkeydown=title.onkeyup=title.onclick=
-    year.onkeydown=year.onkeyup=year.onclick=eventHandler;
+        lastname.onkeydown=lastname.onkeyup=lastname.onclick=
+        title.onkeydown=title.onkeyup=title.onclick=
+        year.onkeydown=year.onkeyup=year.onclick=eventHandler;
     }
 
     setupUpdater();
