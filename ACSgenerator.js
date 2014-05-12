1420798e812315c30fd3cad290ee326fa118ecda
TheAtomicOption <orbenn@hotmail.com>
2014-05-12 12:14:44 -0700

    Added italics and bold
    refactored code a bit.
    switched to use .innerHTML to support html tags

--- ACSgenerator.js
+++ ACSgenerator.js
@@ -1,19 +1,37 @@
     /*
-    * Sourced js text preview functions from: http://inimino.org/~inimino/blog/javascript_live_text_input
+    * Sourced js text preview structure from: http://inimino.org/~inimino/blog/javascript_live_text_input
+    * Also got a lot of help from google links to StackOverflow. Seriously who codes without copying them anymore?
+    * ############ Begin Utility functions ################
     */
 
-    function reverse(s){return s.split('').reverse().join('')}
-
+    //replaces null and blank values with replacement argument
     function replaceNull(test,replacement){
-    if(test == -1) return replacement; else return test;
+    if(test == -1 || test == null || test == "") return replacement; else return test;
     }
 
-    function set(el,text){
-    while(el.firstChild)el.removeChild(el.firstChild);
-    el.appendChild(document.createTextNode(text))}
+    //sets the text of a tag
+    //function set(el,text){
+    //while(el.firstChild)el.removeChild(el.firstChild);
+    //el.appendChild(document.createTextNode(text));}
+    //above works, but can't handle html tags
+    function set(el, text) {
+        while (el.firstChild) el.removeChild(el.firstChild);
+        el.innerHTML = text;
+    }
+    function cleanup() {
+        var cl_year = document.getElementById('year');
 
-    /* setupUpdater will be called once, on page load.*/
+        if (isNaN(cl_year.value.substr(0,4))) { cl_year.value = ''; }
+        if (cl_year.value.length > 4) { cl_year.value = cl_year.value.substr(0, 4); }
 
+        var cl_ayear = document.getElementById('AYear');
+
+        if (isNaN(cl_ayear.value.substr(0, 4))) { cl_ayear.value = ''; }
+        if (cl_ayear.value.length > 4) { cl_ayear.value = cl_ayear.value.substr(0, 4); }
+    }
+    //################ End Utility Functions ###################
+
+/* setupUpdater will be called once, on page load.*/
     function setupUpdater(){
         var lastname=document.getElementById('lastname')
         , title = document.getElementById('title')
@@ -22,6 +40,9 @@
         , month = document.getElementById('month')
         , volume = document.getElementById('volume')
         , pages = document.getElementById('pages')
+        , url = document.getElementById('url')
+        , AYear = document.getElementById('AYear')
+        , AMonth = document.getElementById('AMonth')
         , r_sourceWS = document.getElementById('sourceWS')
         , r_sourceJO = document.getElementById('sourceJO')
         , r_sourceJP = document.getElementById('sourceJP')
@@ -36,13 +57,51 @@
       typing. */
         
         function handleChange() {
-            r_sourceWS.checked ? document.getElementById('div_url').removeAttribute('hidden') : document.getElementById('div_url').setAttribute('hidden',1);
-                //Full citation value
-            var newCitation = lastname.value + ' ' + title.value + '. '
+            
+            cleanup();
+            //Full citation value
+            var newCitation = '';
+            if (r_sourceWS.checked) {
+                //hide/unhide needed fields
+                document.getElementById('div_url').removeAttribute('hidden');
+                document.getElementById('div_AYear').removeAttribute('hidden');
+                document.getElementById('div_AMonth').removeAttribute('hidden');
+
+                newCitation = lastname.value + ' ' + title.value + '. '
                 + journal.value + ' '
-                + ((r_sourceJO.checked) ? '[Online] ' : ' ')
+                + replaceNull(url.value, "{Missing URL}")
+                + ' (accessed ' + AMonth.value + ', ' + AYear.value + ')';
+
+            }
+            else if (r_sourceJP.checked) {
+                //hide/unhide needed fields
+                document.getElementById('div_url').setAttribute('hidden', 1);
+                document.getElementById('div_AYear').setAttribute('hidden', 1);
+                document.getElementById('div_AMonth').setAttribute('hidden', 1);
+
+                newCitation = lastname.value + ' ' + title.value + '. '
+                + '<i>' + journal.value + '</i> '
                 + month.value + ' '
-                + year.value;
+                + '<b>' + year.value + '</b>, '
+                + '<i>' + volume.value + '</i>, '
+                + pages.value + ' ';
+            }
+            else if (r_sourceJO.checked) {
+                //hide/unhide needed fields
+                document.getElementById('div_url').removeAttribute('hidden');
+                document.getElementById('div_AYear').removeAttribute('hidden');
+                document.getElementById('div_AMonth').removeAttribute('hidden');
+
+                newCitation = lastname.value + ' ' + title.value + '. '
+                + '<i>' + journal.value + '</i> '
+                + '[Online] '
+                + month.value + ' '
+                + '<b>' + year.value + '</b>, '
+                + '<i>' + volume.value + '</i>, '
+                + pages.value + ' '
+                + replaceNull(url.value, "{Missing URL}")
+                + ' (accessed ' + AMonth.value + ', ' + AYear.value + ')';
+            }
 
               if (newCitation==oldText) return; else oldText=newCitation;
               set(FullCitation, newCitation);
@@ -76,7 +135,10 @@
             year.onkeydown = year.onkeyup = year.onclick =
             month.onkeydown = month.onkeyup = month.onclick =
             volume.onkeydown = volume.onkeyup = volume.onclick =
+            url.onkeydown = url.onkeyup = url.onclick =
             r_sourceJO.onclick = r_sourceJP.onclick = r_sourceWS.onclick =
+            AMonth.onclick = AMonth.onkeydown = AMonth.onkeyup =
+            AYear.onclick = AYear.onkeydown = AYear.onkeyup =
             pages.onkeydown = pages.onkeyup = pages.onclick = eventHandler;
     }
 
